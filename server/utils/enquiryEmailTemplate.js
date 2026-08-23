ffa50987bb7f93b4c9fac491ee61e2c2197866d5
abhinandanp359 <abhinandanp359@gmail.com>
2026-08-23 16:56:52 +0530

    fix: align reply button and update root package.json scripts

--- server/utils/enquiryEmailTemplate.js
+++ server/utils/enquiryEmailTemplate.js
@@ -189,11 +189,11 @@ const buildEnquiryEmail = ({
       <td style="padding:26px 32px 30px 32px;" align="left">
         <table role="presentation" cellpadding="0" cellspacing="0" border="0" width="100%">
           <tr>
-            <td class="mcs-stack" style="padding:0 10px 10px 0;">
+            <td class="mcs-stack" align="left" style="padding:0 10px 10px 0; width: 50%;">
               ${bulletproofButton({ href: viewUrl, label: 'View Enquiry', variant: 'solid' })}
             </td>
             ${norm.enquiry.email ? `
-            <td class="mcs-stack" style="padding:0;">
+            <td class="mcs-stack" align="right" style="padding:0; width: 50%;">
               ${bulletproofButton({ href: `mailto:${norm.enquiry.email}`, label: 'Reply', variant: 'outline' })}
             </td>` : ''}
           </tr>
